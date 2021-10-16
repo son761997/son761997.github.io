74b8838e06c2479ce5f2ddff3eeb3e9c15e2f48b
PhungHongSon <sonphunghong@gmail.com>
2021-10-16 16:24:18 +0700

    commit fix javalesson1 :3

--- javalesson1/first-app/src/App.java
+++ javalesson1/first-app/src/App.java
@@ -9,7 +9,7 @@ public class App {
 
         System.out.println("\n user's BMI(double): " + user1.calculateBMIdouble());
 
-        System.out.println("\n user's BMI (input double) " + user1.inputcalculateBMIdouble(102, 1.8));
+        System.out.println("\n user's BMI (input double) " + user1.inputcalculateBMIdouble(105, 1.8));
 
        
     }
